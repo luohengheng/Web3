78ae9744da7c30714d4fc53d6a163de6d8da0545
luohengheng <18311473541@163.com>
2025-02-14 14:21:49 +0800

    添加作业

--- Level0/solidity经典用例/Test.sol
+++ Level0/solidity经典用例/Test.sol
@@ -1,13 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract Test {
-    
-    function a() public {
-        bytes32 b1 = blockhash(100);
-        address addr = block.coinbase;
-        uint timestamp = block.timestamp;
-        uint remainingGas = gasleft();
+library MathLib {
+    function add(uint a, uint b) internal pure returns (uint) {
+        return a + b;
+    }
+
+    function subtract(uint a, uint b) internal pure returns (int) {
+        return int(a - b);
+    }
+
+    function multiply(uint a, uint b) internal pure returns (uint) {
+        return a  *  b;
+    }
+
+    function isArrIncludeItem(uint[] memory self, uint _i) external pure returns(bool bol) {
+        for (uint i = 0; i < self.length; i++) {
+            if (i == _i) {
+                bol = true;
+                break;
+            }
+        }
+        bol = false;
     }
 }
 
+contract Test {
+    using MathLib for uint[];
+
+    function test1() external pure returns(uint, uint, int, bool){
+        uint res1 = MathLib.add(11, 22);
+        int res2 = MathLib.subtract(11, 22);
+        uint res3 = MathLib.multiply(11, 22);
+        uint[] memory uArr = new uint[](3);
+        uArr[0] = 1;
+        uArr[1] = 2;
+        uArr[2] = 3;
+        bool res4 = uArr.isArrIncludeItem(1);
+        return (res1, res3, res2, res4);
+    }
+}
